00f172dad852fc4f9ae9b9df5e0e36387f36dd6d
Eslam Osama <106819700+eslam-dv@users.noreply.github.com>
2025-08-09 11:12:30 +0300

    upload files

---
+++ js/app.js
@@ -0,0 +1,48 @@
+const menuIcon = document.getElementById("menuIcon");
+const navMenu = document.getElementById("navMenu");
+const navLinks = navMenu.querySelectorAll("a");
+const projectsContent = document.getElementById("ptojects-content");
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("open");
+  navMenu.classList.toggle("open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menuIcon.classList.remove("open");
+    navMenu.classList.remove("open");
+  });
+});
+
+const projectEl = ({ image, title, tech, description, preview, github }) =>
+  `
+  <div class="project">
+    <img src="${image}" alt="" />
+    <h4 class="title">${title}</h4>
+    <div class="tech">
+      ${tech.map((item) => `<span>${item}</span>`).join("")}
+    </div>
+    <p class="description">
+      ${description}
+    </p>
+    <div class="project-links">
+      <a href="${preview}" id="btn"
+        ><i class="fa-solid fa-arrow-up-right-from-square"></i>
+        Preview</a
+      >
+      <a href="${github}" id="btn"><i class="fa-brands fa-github"></i> Code</a>
+    </div>
+  </div>
+  `;
+
+fetch("../projects.json")
+  .then((response) => {
+    if (!response.ok) {
+      return;
+    }
+    return response.json();
+  })
+  .then((data) => {
+    data.map((project) => (projectsContent.innerHTML += projectEl(project)));
+  });
